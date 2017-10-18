fae0c86f2b00360dc4eae023eb3c2d9078631c69
Diorelyon Amador <diorel_x@hotmail.com>
2017-10-18 10:33:28 -0500

    Modulo de acceso
    se integra modulo sin validacion de correo

---
+++ AccesoBolsaTrabajo/AccesoBolsaTrabajo/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AccesoBolsaTrabajo
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
